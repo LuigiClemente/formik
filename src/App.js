dea98b36ccfd97ff2ade57a5af4fe9dcd378a8d3
Your Name <mrluigiclemente@gmail.com>
2023-04-18 23:54:48 +0200

    Commit message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlightClaimForm from './FlightClaimForm';
+
+function App() {
+  return (
+    <div>
+      <FlightClaimForm />
+    </div>
+  );
+}
+
+export default App;
